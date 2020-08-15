bcfb30d201cdc8fdf99cc6660073558ad8920ea2
github 0322keisuke <isikeidesk@gmail.com>
2020-08-15 17:14:17 +0900

    DBのデータを表示

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -2,36 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Todo;
 use Illuminate\Http\Request;
 
 class TodoController extends Controller
 {
     public function index()
     {
-        // ダミーデータ
-        $todos = [
-        (object) [
-            'id' => 1,
-            'title' => 'タイトル1',
-            'status' => '状態1',
-            'due_date' => '期限日１',
-            'created_at' => now(),
-        ],
-        (object)  [
-            'id' => 2,
-            'title' => 'タイトル2',
-            'status' => '状態2',
-            'due_date' => '期限日2',
-            'created_at' => now(),
-        ],
-        (object)  [
-            'id' => 3,
-            'title' => 'タイトル3',
-            'status' => '状態3',
-            'due_date' => '期限日3',
-            'created_at' => now(),
-        ],
-    ];
+        $todos = Todo::all()->sortByDesc('created_at');
         return view('todos.index', ['todos' => $todos]);
     }
 }
